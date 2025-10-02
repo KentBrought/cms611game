57c42a81c611c1518549c2a1e18e957b23f3dabe
elisaxia <elisaxia@mit.edu>
2025-10-01 20:16:46 -0400

    moving sprite (buggy, doesn't move one cell at a time)

--- Assets/Scripts/UI/Board/BoardManager.cs
+++ Assets/Scripts/UI/Board/BoardManager.cs
@@ -1,45 +1,70 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-
+public class CellData
+{
+    public bool Passable;
+}
 public class BoardManager : MonoBehaviour
 {
-   private Tilemap m_Tilemap;
+    private Tilemap m_Tilemap;
+    private CellData[,] m_BoardData;
+
+    public int Width;
+    public int Height;
+    public Tile[] GroundTiles;
+    public Tile[] WallTiles;
+    private Grid m_Grid;
 
 
-   public int Width;
-   public int Height;
-   public Tile[] GroundTiles;
-   public Tile[] WallTiles;
-
-
-   // Start is called before the first frame update
-   void Start()
+    // Start is called before the first frame update
+    public PlayerController Player;
+    public Vector3 CellToWorld(Vector2Int cellIndex)
     {
-        m_Tilemap = GetComponentInChildren<Tilemap>();
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
 
-        m_BoardData = new CellData[Width, Height];
+// Start is called before the first frame update
+    void Start()
+    {
+    m_Tilemap = GetComponentInChildren<Tilemap>();
+    m_Grid = GetComponentInChildren<Grid>();
+    
+    m_BoardData = new CellData[Width, Height];
 
-        for (int y = 0; y < Height; ++y)
+    for (int y = 0; y < Height; ++y)
+    {
+        for(int x = 0; x < Width; ++x)
         {
-            for(int x = 0; x < Width; ++x)
+            Tile tile;
+            m_BoardData[x, y] = new CellData();
+
+            if(x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
             {
-                Tile tile;
-                m_BoardData[x, y] = new CellData();
-
-                if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
-                {
-                    tile = WallTiles[Random.Range(0, WallTiles.Length)];
-                    m_BoardData[x, y].Passable = false;
-                }
-                else
-                {
-                    tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
-                    m_BoardData[x, y].Passable = true;
-                }
-
-                m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                m_BoardData[x, y].Passable = false;
             }
+            else
+            {
+                tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                m_BoardData[x, y].Passable = true;
+            }
+
+            m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
         }
+    }
+
+    Player.Spawn(this, new Vector2Int(1, 1));
+    }
+    
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= Width
+            || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
+
+        return m_BoardData[cellIndex.x, cellIndex.y];
     }
 }
